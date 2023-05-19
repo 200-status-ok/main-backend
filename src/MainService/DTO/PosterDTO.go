6cb7fd6f53180a92ee7d8ae6788155f3667e1dde
Mohammad Darmanlou <mo.darmanlou@gmail.com>
2023-05-19 19:39:21 +0330

    fix: Fix min tag bug in createPoster

--- src/MainService/DTO/PosterDTO.go
+++ src/MainService/DTO/PosterDTO.go
@@ -1,10 +1,10 @@
 package DTO
 
 type CreatePosterDTO struct {
-	Title       string  `json:"title" binding:"required,min=5,max=255"`
-	Description string  `json:"description" binding:"min=5,max=1000"`
+	Title       string  `json:"title" binding:"required,max=255"`
+	Description string  `json:"description" binding:"max=1000"`
 	Status      string  `json:"status" binding:"required,oneof=lost found"`
-	TelID       string  `json:"tel_id" binding:"min=5,max=255"`
+	TelID       string  `json:"tel_id" binding:"max=255"`
 	UserPhone   string  `json:"user_phone" binding:"min=11,max=13"`
 	Alert       bool    `json:"alert" binding:"required"`
 	Chat        bool    `json:"chat" binding:"required"`
