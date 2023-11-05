a9aff1f3f6ac9991695bd58bd654b59360d31721
MohammadaliFakhari <alifakhary622@gmail.com>
2023-11-05 17:44:09 +0330

    fix: resolve problem in login with google account

--- src/services/MainService/Utils/GoogleAuth.go
+++ src/services/MainService/Utils/GoogleAuth.go
@@ -8,28 +8,38 @@ import (
 	"golang.org/x/oauth2/google"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 var googleClientId = utils.ReadFromEnvFile(".env", "GOOGLE_CLIENT_ID")
 var googleClientSecret = utils.ReadFromEnvFile(".env", "GOOGLE_CLIENT_SECRET")
 
-var googleOauthConfig = &oauth2.Config{
-	RedirectURL:  "https://main-backend.iran.liara.run/api/v1/users/auth/google/callback",
-	ClientID:     googleClientId,
-	ClientSecret: googleClientSecret,
-	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-	Endpoint:     google.Endpoint,
+func GetGoogleOauthConfig() *oauth2.Config {
+	redirectGoogleUrl := ""
+	if os.Getenv("APP_ENV2") == "production" {
+		redirectGoogleUrl = utils.ReadFromEnvFile(".env", "PRODUCTION_REDIRECT_GOOGLE_URL")
+	} else {
+		redirectGoogleUrl = utils.ReadFromEnvFile(".env", "LOCAL_REDIRECT_GOOGLE_URL")
+	}
+	var googleOauthConfig = &oauth2.Config{
+		RedirectURL:  redirectGoogleUrl,
+		ClientID:     googleClientId,
+		ClientSecret: googleClientSecret,
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+	return googleOauthConfig
 }
 
 func GetGoogleAuthURL(state string) string {
-	return googleOauthConfig.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	return GetGoogleOauthConfig().AuthCodeURL(state, oauth2.AccessTypeOffline)
 }
 
 func GetGoogleUserInfo(code string, state string) ([]byte, error) {
 	if state != "random-state" {
 		return nil, errors.New("invalid oauth state")
 	}
-	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	token, err := GetGoogleOauthConfig().Exchange(context.Background(), code)
 	if err != nil {
 		return nil, errors.New("code exchange wrong: " + err.Error())
 	}
